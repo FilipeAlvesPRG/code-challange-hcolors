b38af38c825e1b9e880ab03bbb0a507bd99a2680
Filipe Alves Braga Rosa <54287231+FilipeAlvesPRG@users.noreply.github.com>
2023-01-21 08:53:43 -0300

    Make the Menu Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import theme from "./theme";
 import { RouterProvider } from 'react-router-dom';
 import router from "./routes";
 import TopBarComponent from "./components/TopBarComponent";
+import MenuMobileComponent from "./components/MenuMobileComponent";
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <TopBarComponent />
+      <MenuMobileComponent />
       <RouterProvider router={router} />
     </ThemeProvider>
 
